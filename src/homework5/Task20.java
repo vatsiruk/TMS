56967d74ee17efab2df77370a8febbbbcda7fbd3
vatsiruk <vatsiruk@gmail.com>
2020-07-24 21:19:19 +0300

    fix Task19, Task20

--- src/homework5/Task20.java
+++ src/homework5/Task20.java
@@ -7,14 +7,14 @@ public class Task20 {
      */
     public static void main(String[] args) {
         System.out.println(getLetters("Имеется строка с текстом. " +
-                "Вывести текст, составленный из последних букв всех слов"));
+                "Вывести текст,составленный из последних букв всех слов"));
     }
 
     private static String getLetters(String text) {
-        String[] words = text.replaceAll("\\p{Punct}", "").split("\\s");
+        String[] words = text.replaceAll("\\p{Punct}+", " ").split("\\s+");
         String letters = "";
-        for (int i = 0; i < words.length; i++) {
-            letters += words[i].charAt(words[i].length() - 1);
+        for (String word : words) {
+            letters += word.charAt(word.length() - 1);
         }
         return letters;
     }
